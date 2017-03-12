31fec9b11c448bb2049b6761a1291024c130c609
Michal Bojanowski <michal2992@gmail.com>
2017-03-12 23:26:06 +0100

    Dynamic content for fse_aircraft_aliases

--- R/fse_aircraft_aliases.R
+++ R/fse_aircraft_aliases.R
@@ -4,11 +4,18 @@
 #'
 #' @template feed
 #'
+#' @return
+#' The object will have the following tags/columns:
+#'
+#' \Sexpr[results=rd,stage=build]{
+#' fse:::rd_data_frame(subset(fse::columns, feed == "aircraft_aliases", select=c("column", "description")))
+#' }
+#'
 #' @export
 fse_aircraft_aliases <- function(...) {
   fse_api(
     query="aircraft",
     search="aliases",
     ...
-  )
+  )$content
 }
