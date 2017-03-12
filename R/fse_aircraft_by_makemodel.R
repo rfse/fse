49e21b4bb931c33e0d51de2bf2e56df29ddd92b7
Michal Bojanowski <michal2992@gmail.com>
2017-03-12 02:48:27 +0100

    Updates to man of fse_aircraft_by_makemodel()

--- R/fse_aircraft_by_makemodel.R
+++ R/fse_aircraft_by_makemodel.R
@@ -1,5 +1,13 @@
 #' Aircraft by MakeModel
 #'
+#' @param makemodel character, aircraft model name, see Details
+#'
+#' @details
+#' To get a list of valid model names that can be submitted to \code{makemodel}
+#' fetch aircraft configs with \code{\link{fse_aicraft_configs}} or go to FSE
+#' system WWW page, "Aircraft" menu, "Purchase aircraft" item and check the
+#' dropdown list after selecting "By model" radio button.
+#'
 #' @template feed
 #'
 #' @export
