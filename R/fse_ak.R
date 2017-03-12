ec043631365a9a4aca72fd5a0d112685aca847cc
Michal Bojanowski <michal2992@gmail.com>
2017-03-12 17:42:41 +0100

    Rewrote fse_ak()

--- R/fse_ak.R
+++ R/fse_ak.R
@@ -5,45 +5,36 @@
 #' Retrieved from an enviroment variable \code{FSE_ACCESS_KEY}.
 #' @export
 fse_ak <- function(key=NULL, verbose=getOption("fse.verbose", TRUE)) {
-  # Use service key or personal key?
-  use_service <- TRUE
-  use_service <- ifelse(
-    Sys.getenv("FSE_USE_SERVICE") != "",
-    as.logical( Sys.getenv("FSE_USE_SERVICE") ),
-    use_service
-  )
-  use_service <- getOption("fse.use_service", use_service)
-  if(verbose) cat(paste("Using", ifelse(use_service, "service", "personal"), "key\n"))
-  var_name <- paste(
-    "FSE",
-    ifelse(use_service, "SERVICE", "PERSONAL"),
-    "KEY",
-    sep="_"
-  )
-  option_name <- paste0(
-    "fse.",
-    ifelse(use_service, "service", "personal"),
-    "_key"
-    )
-
-  # Fetch key from environment variable
-  if(verbose) cat("Checking environment", var_name, "\n")
-  thekey <- ifelse(
-    Sys.getenv(var_name) != "",
-    Sys.getenv(var_name),
-    getOption(option_name, "")
-  )
-
-  # Use function argument
-  if(is.null(key)) {
-    rval <- thekey
+  if( !is.null(key) ) {
+    return(key)
   } else {
-    rval <- if(is.null(key)) "" else key
+    # Use service key or personal key?
+    use_service <- TRUE
+    use_service <- ifelse(
+      Sys.getenv("FSE_USE_SERVICE") != "",
+      as.logical( Sys.getenv("FSE_USE_SERVICE") ),
+      use_service
+    )
+    use_service <- getOption("fse.use_service", use_service)
+    if(verbose) cat(paste("Using", ifelse(use_service, "service", "personal"), "key\n"))
+    var_name <- paste(
+      "FSE",
+      ifelse(use_service, "SERVICE", "PERSONAL"),
+      "KEY",
+      sep="_"
+    )
+    option_name <- paste0(
+      "fse.",
+      ifelse(use_service, "service", "personal"),
+      "_key"
+    )
   }
 
-  if(rval == "") {
-    stop("No key. Set in the environment or option, or use `key` argument")
+  key <- getOption(option_name, Sys.getenv(var_name))
+
+  if (key == "" || is.null(key)) {
+    stop("no FSE key, set in the environment or option, or use `key` argument")
   }
 
-  return(rval)
+  return(key)
 }
