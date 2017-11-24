ce5ed2995bc952094870e2ecf89cab5db06d0508
Michal Bojanowski <michal2992@gmail.com>
2017-11-24 23:07:35 +0100

    Testing for aircraft of other users

--- tests/testthat/test-fse_feeds.R
+++ tests/testthat/test-fse_feeds.R
@@ -9,7 +9,7 @@ test_that("fse_aircraft_by_owner_name works silently", {
 
 test_that("fse_aircraft_by_owner_name gives error for nonexisting user", {
   expect_error(
-    r <- fse_aircraft_by_owner_name("foo")
+    r <- fse_aircraft_by_owner_name("khaught")
   )
 })
 
