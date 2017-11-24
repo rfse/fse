18b41fdd3dabe59b8aeea7e6ab2da61c4ca50a2a
Michal Bojanowski <michal2992@gmail.com>
2017-11-24 20:21:12 +0100

    Tests of fse_aircraft_by_owner_name
    Closes #3

---
+++ tests/testthat/test-fse_feeds.R
@@ -0,0 +1,15 @@
+context("Testing fse_aircraft_by_owner_name")
+
+
+test_that("fse_aircraft_by_owner_name works silently", {
+  expect_silent(
+    r <- fse_aircraft_by_owner_name("skybike")
+  )
+})
+
+test_that("fse_aircraft_by_owner_name gives error for nonexisting user", {
+  expect_error(
+    r <- fse_aircraft_by_owner_name("foo")
+  )
+})
+
