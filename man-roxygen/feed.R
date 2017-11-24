6c817830c564a79180931100b19e593150cf6b97
Michal Bojanowski <michal2992@gmail.com>
2017-11-24 23:07:03 +0100

    Feed template dynamically displays data documentation

--- man-roxygen/feed.R
+++ man-roxygen/feed.R
@@ -2,6 +2,9 @@
 #'
 #' @return
 #' Feed can return \code{data.frame} (default) or \code{xml_document} depending
-#' on the value of \code{format} (see \code{\link{fse_api}}).
+#' on the value of \code{format} (see \code{\link{fse_api}}). In either case the result will
+#' contain the following columns/tags:
 #'
-#' @family fse_feeds
+#' \Sexpr[results=verbatim,stage=render,echo=FALSE]{try(fse:::column_descriptions('<%=if(exists("feedname")) feedname%>'))}
+#'
+#' @family FSE feeds
