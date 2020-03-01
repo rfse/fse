bad43fa5292739d5e57cfd2d19d02403d61ee9d5
Michal Bojanowski <michal2992@gmail.com>
2020-03-01 23:44:47 +0100

    Do not check for API errors (for now)

--- R/fse_api.R
+++ R/fse_api.R
@@ -92,10 +92,6 @@ fse_api <- function(
     )
   }
 
-  tt <- content(resp, "text", encoding = "UTF-8")
-  xml_orig <- xml2::read_xml(tt)
-  fse_errors(xml_orig)
-
   # Check response type
   resp_type <- http_type(resp)
   expect_type <- c(
