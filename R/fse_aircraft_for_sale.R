749be3aa16e773a4081ec57283ffd7d559c4b2bd
Michal Bojanowski <michal2992@gmail.com>
2020-03-11 00:26:30 +0100

    Proper use of template in aircraft_for_sale

--- R/fse_aircraft_for_sale.R
+++ R/fse_aircraft_for_sale.R
@@ -1,14 +1,7 @@
 #' Aircraft for Sale
 #'
+#' @templateVar feedname aircraft_for_sale
 #' @template feed
-#'
-#' @return
-#' The object will have the following tags/columns:
-#'
-#' \Sexpr[results=rd,stage=build]{
-#' fse:::rd_data_frame(subset(fse::columns, feed == "aircraft_for_sale", select=c("column", "description")))
-#' }
-#'
 #'
 #' @export
 fse_aircraft_for_sale <- function(...) {
