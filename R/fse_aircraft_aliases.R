1a76ece548e6d36d96844f6df39a77d879523e02
Michal Bojanowski <michal2992@gmail.com>
2020-03-10 21:51:43 +0100

    Fix man of fse_aircraft_aliases()

--- R/fse_aircraft_aliases.R
+++ R/fse_aircraft_aliases.R
@@ -2,14 +2,8 @@
 #'
 #' Aliases of FSE aircraft.
 #'
+#' @templateVar feedname aircraft_aliases
 #' @template feed
-#'
-#' @return
-#' The object will have the following tags/columns:
-#'
-#' \Sexpr[results=rd,stage=build]{
-#' fse:::rd_data_frame(subset(fse::columns, feed == "aircraft_aliases", select=c("column", "description")))
-#' }
 #'
 #' @export
 fse_aircraft_aliases <- function(...) {
