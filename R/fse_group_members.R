3978b257ba6da8f2e211d3b8a7afc004bc77e8e4
Michal Bojanowski <michal2992@gmail.com>
2020-03-31 20:25:02 +0200

    Add fse_group_members()
    Closes #19.

---
+++ R/fse_group_members.R
@@ -0,0 +1,18 @@
+#' List group members
+#'
+#' List members of a FSE group identified by the `accesskey`
+#'
+#' @param accesskey Read Access Key of a FSE group
+#'
+#' @templateVar feedname icao_fbos
+#' @template feed
+#'
+#' @export
+fse_group_members <- function(accesskey, ...) {
+  fse_api(
+    query="group",
+    search="members",
+    readaccesskey = accesskey,
+    ...
+  )$content
+}
