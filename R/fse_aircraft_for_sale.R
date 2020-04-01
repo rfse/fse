ca2b11fab8ccd47a71eab785394f60fb4303cd95
Michal Bojanowski <michal2992@gmail.com>
2020-04-01 21:24:05 +0200

    Document columns in returned object

--- R/fse_aircraft_for_sale.R
+++ R/fse_aircraft_for_sale.R
@@ -1,9 +1,42 @@
 #' Aircraft for Sale
 #'
-#' @templateVar feedname aircraft_for_sale
-#' @template feed
+#' List of aircraft currently for sale.
+#'
+#' @template feed2
+#'
+#' @return The return object contains the following columns/tags:
+#'
+#' @return - `SerialNumber`
+#' - `MakeModel`
+#' - `Registration`
+#' - `Owner`
+#' - `Location`
+#' - `LocationName`
+#' - `Home`
+#' - `SalePrice`
+#' - `SellbackPrice`
+#' - `Equipment`
+#' - `RentalDry`
+#' - `RentalWet`
+#' - `RentalType`
+#' - `Bonus`
+#' - `RentalTime`
+#' - `RentedBy`
+#' - `PctFuel`
+#' - `NeedsRepair`
+#' - `AirframeTime`
+#' - `EngineTime`
+#' - `TimeLast100hr`
+#' - `LeasedFrom`
+#' - `MonthlyFee`
+#' - `FeeOwed`
 #'
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#' r <- fse_aircraft_for_sale()
+#' }
 fse_aircraft_for_sale <- function(...) {
   fse_api(
     query="aircraft",
