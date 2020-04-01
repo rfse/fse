b77141a365ff0b819a8af9759abb8076a56b3cb6
Michal Bojanowski <michal2992@gmail.com>
2020-04-01 21:27:06 +0200

    Add example to fse_aircraft_configs()

--- R/fse_aircraft_configs.R
+++ R/fse_aircraft_configs.R
@@ -6,6 +6,11 @@
 #' @template feed
 #'
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#' r <- fse_aircraft_configs()
+#' }
 fse_aircraft_configs <- function(...) {
   res <- fse_api(
     query="aircraft",
