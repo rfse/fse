46eb369d1637668459037532e27efda608fd9478
Michal Bojanowski <michal2992@gmail.com>
2020-04-02 16:15:23 +0200

    Document columns for fse_assignments_by_key()

--- R/fse_assignments_by_key.R
+++ R/fse_assignments_by_key.R
@@ -4,8 +4,26 @@
 #'
 #' @param accesskey FSE read access key
 #'
-#' @templateVar feedname assignments_by_key
-#' @template feed
+#' @template feed2
+#'
+#' @return The object contains the following columns/tags:
+#'
+#' @return - `Id` -- Assignment Id
+#' - `Status` -- Assignment status one of "Selected", "Departing" or "Enroute"
+#' - `Location` -- ICAO where the job is at
+#' - `From` -- Originating airport
+#' - `Destination` -- Destination airport
+#' - `Assignment` -- Description of payload
+#' - `Amount` -- Number of pax or kg
+#' - `Units` -- The unit of `Amount`
+#' - `Pay` -- Assignment pay in vdollars
+#' - `PilotFee` -- Pilot fee (for group assignments?)
+#' - `Expires` -- Amount of time in which the assignment will expire
+#' - `ExpireDateTime` -- Date and time at which the assignment will expire
+#' - `Type` -- Type of assignment: "Trip-Only", "All-In"...
+#' - `Express` -- Is it an express assignment
+#' - `Locked` -- Name of the pilot that has the assignment selected
+#' - `Comment` -- Additional comments
 #'
 #' @export
 
