9656136258676ae0bd7c88e56a88b81bb8753aa8
Michal Bojanowski <michal2992@gmail.com>
2020-04-02 19:09:42 +0200

    Rewrite fse_ak() tests

--- tests/testthat/test-fse_ak.R
+++ tests/testthat/test-fse_ak.R
@@ -1,52 +1,71 @@
+context("Retrieving access keys")
 
-context("Testing fetching access key")
-
+# Testing environment variables
 e <- c(
-  FSE_USE_SERVICE="0",
   FSE_SERVICE_KEY = "servicekey",
   FSE_PERSONAL_KEY = "personalkey"
 )
 
-test_that("fse_ak fetches properly based on environment variable", {
+# As fse_key objects
+k <- list(
+  sk = as_fse_key(e["FSE_SERVICE_KEY"], type="service"),
+  pk = as_fse_key(e["FSE_PERSONAL_KEY"], type="personal")
+)
+
+
+
+
+
+test_that("fse_ak on 'auto' fetches appropriate key when available", {
+  # All available, then SK
   withr::with_envvar(
-    replace(e, 1, "0"),
+    e,
     expect_equivalent(
       fse_ak(),
-      as_fse_key(e["FSE_PERSONAL_KEY"], type="personal")
+      k$sk
     )
   )
 
+  # Only PK available, then PK
   withr::with_envvar(
-    replace(e, 1, "1"),
+    replace(e, 1, as.character(NA)),
     expect_equivalent(
       fse_ak(),
-      as_fse_key(e["FSE_SERVICE_KEY"], type="service")
+      k$pk
       )
+  )
+
+  # None availble, then error
+  withr::with_envvar(
+    replace(e, 1:2, as.character(NA)),
+    expect_error(fse_ak())
   )
 })
 
 
 
 
-test_that("fse_ak fetches properly based on R option", {
+test_that("proper key is fetched based on option", {
+  # Personal
   withr::with_envvar(
     e,
     withr::with_options(
-      list(fse.use_service = 0),
+      list(fse.key_type = "personal"),
       expect_equivalent(
         fse_ak(),
-        as_fse_key(e["FSE_PERSONAL_KEY"], "personal")
-        )
+        k$pk
+      )
     )
   )
 
+  # Service
   withr::with_envvar(
     e,
     withr::with_options(
-      list(fse.use_service = 1),
+      list(fse.key_type = "service"),
       expect_equivalent(
         fse_ak(),
-        as_fse_key(e["FSE_SERVICE_KEY"], type="service")
+        k$sk
       )
     )
   )
