b327e9863e75d260e3db8f658e629ca88806e920
Michal Bojanowski <michal2992@gmail.com>
2020-04-03 20:28:04 +0200

    Improve man of fse_aircraft_configs()

--- R/fse_aircraft_configs.R
+++ R/fse_aircraft_configs.R
@@ -6,10 +6,10 @@
 #'
 #' @template feed2
 #'
-#' @return Object returned contains the following columns/tags:
-#'
-#' @return - `Crew`
-#' - `Seats`
+#' @return Function [fse_aircraft_configs()] return a data frame or XML object
+#'   with the columns/tags listed in the Format section. Object
+#'   [aircraft_configs] is simply a data frame returned by that function on
+#'   2020-04-03.
 #'
 #' @export
 #'
@@ -32,5 +32,31 @@ fse_aircraft_configs <- function(...) {
 
 #' @rdname fse_aircraft_configs
 #'
-#' @format Data frame with aircraft configuration.
+#' @format Object `aircraft_configs` is a data frame returned by
+#'   [fse_aircraft_configs()] and has the following columns:
+#'
+#' @format  - `MakeModel` -- Aircraft model name
+#' - `Crew` -- Number of crew members
+#' - `Seats` -- Number of PAX seats
+#' - `CruiseSpeed` -- Average cruise speed
+#' - `GPH` -- Fuel consumption in Gallons Per Hour
+#' - `FuelType` -- Fuel type
+#' - `MTOW` -- Maximal TakeOff Weight
+#' - `EmptyWeight` -- Empty weight
+#' - `Price` -- System aircraft price
+#' - `Ext1` -- Fuel tank capacity: external 1
+#' - `LTip` --
+#' - `LAux` --
+#' - `LMain` --
+#' - `Center1` --
+#' - `Center2` --
+#' - `Center3` --
+#' - `RMain` --
+#' - `RAux` --
+#' - `RTip` --
+#' - `RExt2` -- Fuel tank capacity: right external 2
+#' - `Engines` -- Number of engines
+#' - `EnginePrice` -- Engine price (maintenance)
+#' - `ModelId` -- Aircraft model ID
+#'
 "aircraft_configs"
